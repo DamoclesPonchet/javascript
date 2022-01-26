16c5b778b454fe56adb6680528aab5a4012f4f1a
damoclesPonchet <97700857+damoclesponchet@users.noreply.github.com>
2022-01-26 11:46:31 -0300

    nov

--- desafios/desafi004/desafiop1/sript.js
+++ desafios/desafi004/desafiop1/sript.js
@@ -2,24 +2,30 @@ function montar(){
     var i = document.getElementById('inicio') 
     var f = document.getElementById('ultimo')
     var s = document.getElementById('saltos')
-    
-    var iv = Number(i.value) 
-    var fv = Number(f.value)
-    var sv = Number(s.value)
-
     var t = document.getElementById('texto')
-    if  (i =="")  {
-        window.alert ('ERRO não a dado na caixa de inicio.')
-    }else if (f == ''){
-        window.alert ('ERRO não a dado na caixa de fim.')
-    }else if (s == ''&& s == 0) {
-        window.alert ('ERRO não a dado na caixa de salto.')
-    }    
-    else{    
-        while (iv <= fv){
-            t.innerHTML += `${iv} &#x1F449`
-            iv +=sv
+    
+    if (p==0){
+        window.alert('não a como dar saltos no valor de 0, lle sera considerado 1.')
+        s = 1
+    }else{
+        if (i.value.length == 0||f.value.length == 0||s.value.length == 0){
+            window.alert('ERRO algum valor não foi definido')
+        }else{
+            t.innerHTML = `Contando:`
+            var iv = Number(i.value) 
+            var fv = Number(f.value)
+            var sv = Number(s.value)
+
+            if (iv > fv){
+                for (var iv ; iv >= fv; iv += sv){
+                    t.innerHTML += `${iv} &#x1F449`
+                }
+            }else{    
+                for (var iv ; iv <= fv; iv += sv){
+                    t.innerHTML += `${iv} &#x1F449`
+                }
+            }    
+                t.innerHTML += `&#x1F3C1`
         }
-    }    
-    t.innerHTML += `&#x1F3C1`
+    }
 }
