89865250f9e5f4baa57219e11c7de16c700133b7
damoclesPonchet <97700857+damoclesponchet@users.noreply.github.com>
2022-01-26 12:47:51 -0300

    nov

--- desafios/desafi004/desafiop2/sript.js
+++ desafios/desafi004/desafiop2/sript.js
@@ -1,13 +1,18 @@
 function tabuada(){
     var n = document.getElementById ('base')
-    var nv = Number(n.value)
-    var p = 1
-    var t = document.getElementById('texto')
-    var v = p *nv
-    t.innerHTML = `tabuada forma:<br>`
-    for(var p ;p < 11 ; p++ ){
-        var v = p *nv
-        t.innerHTML += `${nv} x ${p} = ${v}<br>`
+    if (n.value.length == 0 ){
+        window.alert  ('ERRO a base não foi definida, escreva algum valor no campo abixo.')
+    }else{
+        var nv = Number(n.value)
+        var p = 1
+        var t = document.getElementById('texto')
+        t.innerHTML = ''
+        for(var p ;p < 11 ; p++ ){
+            var l = document.createElement('option')
+            var v = p *nv
+            l.text += `${nv} x ${p} = ${nv*p}`
+            l.value = `tab${p}`
+            t.appendChild(l)
+        }
     }
-    cax.innerHTML += `----------------- <br>`
 }
