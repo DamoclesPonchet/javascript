5ed1da947e40cb0ffe66f938df9476e5481ac78d
damoclesPonchet <97700857+damoclesponchet@users.noreply.github.com>
2022-01-30 08:05:35 -0300

    Update javascrip.js

--- desafios/desafi005/javascrip.js
+++ desafios/desafi005/javascrip.js
@@ -24,6 +24,7 @@ function adicionar(){
         var item = document.createElement('option')
         item.text = `${num.value} adicionado`
         lista.appendChild(item)
+        caixa.innerHTML = ``
     }else{
         window.alert('ERRO seu valor passou do intervalo ou ele já foi escrito')
     }
@@ -35,7 +36,24 @@ function final(){
         window.alert('ERRO não ha nenhum valor cadastrado.')
     }else{
         var tot = valores.length
-
-        res
+        maior = valores[0]
+        menor = valores[0]
+        soma = 0
+        media = 0
+        for (var i in valores){
+            soma += valores[i]
+            if (valores[i] >maior){
+                maior = valores[i]
+            }else if (valores[i] <menor) {
+                menor = valores[i]
+            }
+        }
+        media = soma / tot
+        caixa.innerHTML = ``
+        caixa.innerHTML += `<p>tem ${tot} números cadastrados</p>`
+        caixa.innerHTML += `<p>o maior número é ${maior}</p>`
+        caixa.innerHTML += `<p>o menor número é ${menor}</p>`
+        caixa.innerHTML += `<p>a soma dos valores é ${soma}</p>`
+        caixa.innerHTML += `<p>a media dos valore é ${media}</p>`
     }
 }
